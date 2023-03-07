7606fd1eabdd993b9037a1f55575fafa1c79f895
Liumyss <92648495+Liumyss@users.noreply.github.com>
2023-03-07 22:21:11 +0100

    habit tile created

---
+++ lib/components/habit_tile.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../constants/color_palette.dart';
+
+class HabitTile extends StatelessWidget {
+  final String habitName;
+  final bool habitCompleted;
+  final Function(bool?)? onChanged;
+
+  const HabitTile({
+    super.key,
+    required this.habitName,
+    required this.habitCompleted,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        padding: const EdgeInsets.all(24),
+        decoration: BoxDecoration(
+          color: secondaryColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: [
+            Checkbox(value: false, onChanged: onChanged),
+            const Text(
+              'Meditation',
+              style: TextStyle(
+                  color: backgroundColor, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -1,37 +1,56 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:habittrackerapp/constants/color_palette.dart';
 import 'package:habittrackerapp/services/auth/auth_service.dart';
 
+import '../components/habit_tile.dart';
 import '../constants/routes.dart';
 
-class MainView extends StatelessWidget {
-  MainView({super.key});
+class MainView extends StatefulWidget {
+  const MainView({super.key});
 
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
   final user = FirebaseAuth.instance.currentUser!;
+
+  bool habitCompleted = false;
+
+  void checkBoxTapped(bool? value) {
+    setState(() {
+      habitCompleted = value!;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () async {
-              await AuthService.firebase().logOut();
-              Navigator.of(context).pushNamedAndRemoveUntil(
-                registerRoute,
-                (route) => false,
-              );
-            },
-            icon: const Icon(Icons.logout),
-          )
-        ],
-      ),
-      body: Center(
-        child: Text(
-          'LOGGED IN AS: ${user.email!}',
-          style: const TextStyle(fontSize: 20),
+        backgroundColor: backgroundColor,
+        appBar: AppBar(
+          backgroundColor: primaryColor,
+          actions: [
+            IconButton(
+              onPressed: () async {
+                await AuthService.firebase().logOut();
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                  registerRoute,
+                  (route) => false,
+                );
+              },
+              icon: const Icon(Icons.logout),
+            )
+          ],
         ),
-      ),
-    );
+        body: ListView(
+          children: [
+            HabitTile(
+              habitName: "Meditation",
+              habitCompleted: habitCompleted,
+              onChanged: (value) => checkBoxTapped(value),
+            ),
+          ],
+        ));
   }
 }
