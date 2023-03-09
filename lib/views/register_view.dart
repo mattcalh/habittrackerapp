64681e16297225a6b4115c8e2e8d151637ce9f72
Liumyss <92648495+Liumyss@users.noreply.github.com>
2023-03-09 15:15:01 +0100

    Small changes

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:habittrackerapp/components/square_tile.dart';
 import 'package:habittrackerapp/constants/routes.dart';
